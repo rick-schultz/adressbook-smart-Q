f461e2bc5c698cd29e78e97f8fc642144cf4d35b
Rick Schultz <rick-schultz@hotmail.com>
2021-09-03 19:01:34 -0300

    database

--- config/db.php
+++ config/db.php
@@ -3,5 +3,5 @@ $con = mysqli_connect("localhost", "root", "");
 if ($con == NULL) {
  echo "Error establishing database connection.";
 } else {
- mysqli_select_db($con, "bmcqwd4mvasj4lk4vk4q");
+ mysqli_select_db($con, "adressbook-smart-Q");
 }

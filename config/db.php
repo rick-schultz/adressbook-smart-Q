1fb94b5ffd6c561a1fc9d0fc8e2712c10cf12c7b
Rick Schultz <rick-schultz@hotmail.com>
2021-09-03 19:03:31 -0300

    database

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
-$con = mysqli_connect("localhost", "root", "");
+$con = mysqli_connect("bmcqwd4mvasj4lk4vk4q-mysql.services.clever-cloud.com", "u6qdlkuzhnaipicv", "BNtvFARKrbOw3VNCCGMi");
 if ($con == NULL) {
  echo "Error establishing database connection.";
 } else {
- mysqli_select_db($con, "adressbook-smart-Q");
+ mysqli_select_db($con, "bmcqwd4mvasj4lk4vk4q");
 }

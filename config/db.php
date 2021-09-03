5686efd6f939f53068fde6ee3fafbcc96cf64b11
Rick Schultz <rick-schultz@hotmail.com>
2021-09-03 18:58:00 -0300

    change database

--- config/db.php
+++ config/db.php
@@ -3,5 +3,5 @@ $con = mysqli_connect("localhost", "root", "");
 if ($con == NULL) {
  echo "Error establishing database connection.";
 } else {
- mysqli_select_db($con, "address-book");
+ mysqli_select_db($con, "bmcqwd4mvasj4lk4vk4q");
 }
